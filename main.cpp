edeea2ed8e7d0aaf422d2e79a61f513db5686cde
judp <judp@users.noreply.github.com>
2018-10-23 06:46:53 +0800

    problem

--- main.cpp
+++ main.cpp
@@ -1,114 +1,60 @@
-#include<cstdio>
+#inlcude<cstdio>
 
-void mv(int f,int x,int y);
-void exchange(int x,int y);
+struct Node
+{
+    bool black;
+    Node* n1,n2,n3,n4;
+};
 
-constexpr int maxn =100001;
-int d[2][maxn];
-bool flag;
-int m,n;
+Node nodes1[341],nodes2[341];
+int cur;
+Node* build(Node*p)
+{
+    char c;
+    c=getchar();
+    Node*pn=p+cur;
+    if(c=='p')
+    {
+        pn->n1=build(p);
+        pn->n2=build(p);
+        pn->n3=build(p);
+        pn->n4=build(p);
+    }
+    else
+    {
+        pn->black=(c=='e'?0:1);
+        pn->n1=nullptr;
+    }
+    return pn;
+}
+int cnt;
+
+void solve(Node root1,Node,root2,int n)
+{
+    if(root1->n1!=nullptr&&root2->n1!=nullptr)
+    {
+        solve(root1->n1,root2->n1);
+        solve(root1->n2,root2->n2);
+        solve(root1->n3,root2->n3);
+        solve(root1->n4,root2->n4);
+    }
+    else if()
+}
 int main()
 {
-    int kase=0;
-    while(scanf("%d%d",&n,&m)==2)
+    int n;
+    scanf("%d",&n);
+    while(n--)
     {
-        flag=0;
-        for(int i=1; i<=n; ++i)
-        {
-            d[0][i]=i+1;
-            d[1][i]=i-1;
-        }
-        d[0][n]=0;
-        d[0][0]=1;
-        d[1][0]=n;
+        Node root1,root2;
+        cur=0;
+        root1=build(nodes1);
+        getchar();
+        cur=0;
+        root2=build(nodes2);
 
-        int cmd,x,y;
-        for(int i=0; i<m; ++i)
-        {
-            scanf("%d",&cmd);
-            --cmd;
-            if(cmd!=3)
-                scanf("%d%d",&x,&y);
-            if(cmd==0||cmd==1)
-                mv(flag^cmd,x,y);
-            else if(cmd==2)
-                exchange(x,y);
-            else if(cmd==3)
-                flag=!flag;
-        }
-        long long sum=0;
-        for(int i=0,cnt=1; d[flag][i]; i=d[flag][i],cnt++)
-        {
-            if(cnt%2)
-                sum+=d[flag][i];
-            //printf("%d   ",d[flag][i]);
-        }
-        printf("Case %d: %lld\n",++kase,sum);
-        //putchar('\n');
+        cnt=0;
+        solve(root1,root2,0);
+        printf("There are %d black pixels.\n",cnt);
     }
 }
-
-void mv(int f,int x,int y)
-{
-    if(!d[f][x] || d[f][x]!=d[!f][y])
-    {
-        d[f][d[!f][x]]=d[f][x];
-
-        if(d[f][x])
-            d[!f][d[f][x]]=d[!f][x];
-        else
-            d[!f][0]=d[!f][x];
-
-        if(d[!f][y])
-            d[f][d[!f][y]]=x;
-        else
-            d[f][0]=x;
-
-        d[!f][x]=d[!f][y];
-
-        d[f][x]=y;
-        d[!f][y]=x;
-    }
-}
-void exchange(int x,int y)
-{
-
-
-    int r=d[flag][y],l=d[!flag][y];
-
-    if(l)
-        d[flag][l]=x;
-    else
-        d[flag][0]=x;
-
-    if(r)
-        d[!flag][r]=x;
-    else
-        d[!flag][0]=x;
-
-    if(d[!flag][x])
-        d[flag][d[!flag][x]]=y;
-    else
-        d[flag][0]=y;
-
-    if(d[flag][x])
-        d[!flag][d[flag][x]]=y;
-    else
-        d[!flag][0]=y;
-
-    d[flag][y]=d[flag][x];
-    d[!flag][y]=d[!flag][x];
-
-    d[flag][x]=r;
-    d[!flag][x]=l;
-}
-
-
-
-
-
-
-
-
-
-
